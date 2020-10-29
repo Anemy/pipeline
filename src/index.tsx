6e2272f90309c46f67d2e1ed8609d38a351f37a4
Anemy <rhys@rhysh@live.com>
2020-10-29 17:31:01 +0100

    Iceberg

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap-grid.css';
-import App from './appp';
+import App from './app';
 
 require('dotenv').config();
 
